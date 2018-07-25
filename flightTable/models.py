9668b5077fc462fc1b478b19091e1a525f42ae47
Miron Szewczyk <miron.szewczyk@autonomik.pl>
2018-07-25 11:16:08 +0200

    Added sorted columns

--- flightTable/models.py
+++ flightTable/models.py
@@ -1,8 +1,10 @@
 import datetime
+
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
@@ -18,6 +20,11 @@ class Plane(models.Model):
     airline = models.ForeignKey(Airline, on_delete=models.CASCADE)
     seats = models.IntegerField(default=20)
 
+    def clean(self):
+        super().clean()
+        if self.seats < 1:
+            raise ValidationError(_("Number of seats must be a positive number."))
+
     def __str__(self):
         return self.registration_num
 
@@ -26,6 +33,8 @@ class Airport(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
+
+
 
 
 class Flight(models.Model):
@@ -37,6 +46,7 @@ class Flight(models.Model):
     # passenger = models.ManyToManyField(Passenger)
     def __str__(self):
         return self.plane.airline.__str__()+ "/"+self.plane.__str__()+" flight nr "+self.id.__str__()
+
 
 
 
--- flightTable/views.py
+++ flightTable/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView
+import datetime
 
+from django.shortcuts import render, redirect, get_object_or_404
 from flightTable.forms import RegistrationForm,AddPassengerToFlightForm
 from django.views.decorators.http import require_POST
 from django.contrib.auth import authenticate, login as login_to_session
 from django.contrib.auth.models import User
-from .models import Flight, Passenger, Airport
+from .models import Flight, Passenger, Airport, Airline, Plane
 
 import django_tables2 as tables
 
@@ -73,9 +73,42 @@ class SimpleTable(tables.Table):
         attrs = {'class': 'table'}
 
 def simple_list(request):
-    queryset = Flight.objects.all()
+    # populate()
+
+    if "sort" in request.GET and request.GET.get("sort", "") != "edit_entries":
+        queryset = Flight.objects.all().order_by(request.GET.get("sort", ""))
+    else:
+        queryset = Flight.objects.all()
+
     table = SimpleTable(queryset)
     table.paginate(page=request.GET.get('page', 1), per_page=25)
     return render(request, 'flightTable/flight_list.html', {'table': table})
 
 
+def populate():
+    airport1 = Airport(name="MIMUW")
+    airport1.save()
+
+    airport2 = Airport(name="PG")
+    airport2.save()
+
+    airport3 = Airport(name="MINIPW")
+    airport3.save()
+
+    airports=[airport1, airport2, airport3]
+
+    for i in range(20):
+        airline = Airline(name="airline nr"+i.__str__())
+        airline.save()
+        plane = Plane(airline=airline, registration_num="rn"+i.__str__(),seats=20+i%11)
+        delta = datetime.timedelta(days=1, hours=i)
+        today = datetime.datetime.now()
+        date = datetime.datetime.now() + delta
+        plane.save()
+        for j in range(20):
+            start = today+j*delta
+            finnish = today+datetime.timedelta(hours=i%12+1,minutes=32*(j%10))
+            flight = Flight(departure_airport=airports[j%3],arrival_airport=airports[(j+1)%3],
+                            departure_time=start,arrival_time=finnish,
+                            plane=plane)
+            flight.save()
